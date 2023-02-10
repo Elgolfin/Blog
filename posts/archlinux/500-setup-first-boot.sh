71ba9c99ba56192e9cdb7d3265feb236c5b7030d
Nico R <nico.rose@sonepar.ca>
2023-02-10 15:09:53 -0500

    Add a command line to configure the wired networks

--- posts/archlinux/500-setup-first-boot.sh
+++ posts/archlinux/500-setup-first-boot.sh
@@ -7,5 +7,15 @@ cat <<EOF >/etc/iwd/main.conf
 EnableNetworkConfiguration=true
 EOF
 
+# Configure wired networks
+touch /etc/systemd/network/20-wired.network
+cat <<EOF >/etc/systemd/network/20-wired.network
+[Match]
+Name=enp1s*
+
+[Network]
+DHCP=yes
+EOF
+
 # Manually configure the Wifi
-iwctl
+# iwctl
